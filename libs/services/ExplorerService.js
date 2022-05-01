a4524cfdc9723b05c84ef8f8d4644574ac0bde1b
ChristianDeM <Christian.d.martino@gmail.com>
2022-04-30 23:18:25 -0500

    6 Creando ExplorerService

---
+++ libs/services/ExplorerService.js
@@ -0,0 +1,24 @@
+const Reader= require("./../utils/Reader")
+const explorer=Reader.readJsonFile("../../explorers.json")
+//console.log(explorer);
+
+class ExplorerService{
+    static filterBymission(explorers,mission){
+        const explorersByMission = explorers.filter((explorer) => explorer.mission === mission);
+         return explorersByMission;
+    }
+    static getAmountOfExplorersByMission(explorers){
+        const explorersInNode = explorers.filter((explorer) => explorer.mission == "node");
+        return explorersInNode;
+    }
+
+    static getExplorersUsernamesByMission(explorers){
+        const explorersInNodeToGetUsernames = explorers.filter((explorer) => explorer.mission == "node");
+        const usernamesInNode = explorersInNodeToGetUsernames.map((explorer) => explorer.githubUsername);
+        return usernamesInNode;
+    }
+}
+
+
+
+module.exports=ExplorerService
