bf8cd59ea5bd211f460e07872347d4600da69481
ChristianDeM <Christian.d.martino@gmail.com>
2022-05-01 00:06:03 -0500

    Test Explorer services  filtro por mision

--- libs/services/ExplorerService.js
+++ libs/services/ExplorerService.js
@@ -3,15 +3,15 @@ class ExplorerService{
         const explorersByMission = explorers.filter((explorer) => explorer.mission === mission);
          return explorersByMission;
     }
-    static getAmountOfExplorersByMission(explorers){
-        const explorersInNode = explorers.filter((explorer) => explorer.mission == "node");
-        return explorersInNode;
-    }
 
     static getExplorersUsernamesByMission(explorers){
         const explorersInNodeToGetUsernames = explorers.filter((explorer) => explorer.mission == "node");
         const usernamesInNode = explorersInNodeToGetUsernames.map((explorer) => explorer.githubUsername);
         return usernamesInNode;
+    }
+    static getAmountOfExplorersByMission(explorers){
+        const explorersInNode = explorers.filter((explorer) => explorer.mission == "node");
+        return explorersInNode;
     }
 }
 
---
+++ test/ExplorerService.test.js
@@ -0,0 +1,11 @@
+const   ExplorerService = require("./../libs/services/ExplorerService")
+const   explorers = require("./exploreres.test.json")
+describe("test  para explorer service",()=>{
+    test("requerimiento 1 obtener lista de explores con node" ,()=>{
+        const ExplorersNode = ExplorerService.filterBymission(explorers,"node");
+        expect(ExplorersNode[0].name).toBe("Woopa1");
+        expect(ExplorersNode[0].githubUsername).toBe("ajolonauta1");
+        expect(ExplorersNode[0].score).toBe(1);
+        expect(ExplorersNode[0].mission).toBe("node");       
+    });
+});
