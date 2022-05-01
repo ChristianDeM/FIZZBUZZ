685c4ad01ec7fe6d36398da6081bba6537d783d6
ChristianDeM <Christian.d.martino@gmail.com>
2022-05-01 02:40:54 -0500

    test Fizzbuzz

--- libs/services/FizzbuzzService.js
+++ libs/services/FizzbuzzService.js
@@ -1,28 +1,26 @@
-const Reader = require("./lib/utils/Reader");
-const explorers = Reader.readJsonFile("explorers.json"); 
-  
 class FizzbuzzService{
+    
     static applyValidationInExplore(explorer){
-        this.explorer=explorer;
-        const assignBuzzTrick = function(explorer){
             if(explorer.score%5 === 0 && explorer.score%3 === 0){
                 explorer.trick = "FIZZBUZZ";
-                return explorer;
+                return explorer
             }
             else if(explorer.score%3 === 0){
                 explorer.trick = "FIZZ";
-                return explorer;
+                return explorer
+
             }
             else if(explorer.score%5 === 0){
                 explorer.trick = "BUZZ";
-                return explorer;
+                return explorer
+
             }
             else{
                 explorer.trick = explorer.score;
-                return explorer;
+                return explorer
+
             }
-        };
-        return assignBuzzTrick(explorer);            
+            
     }  
 }
-module.exports = FizzbuzzService;
+module.exports =  FizzbuzzService;
---
+++ test/FizzbuzzService.test.js
@@ -0,0 +1,13 @@
+const FizzbuzzService = require("../libs/services/FizzbuzzService");
+const   explorer = require("./exploreres.test.json")
+
+describe("test  para Fizzbuzz service",()=>{
+    test("requerimiento 1 divisble entre 3" ,()=>{   
+        const explorer1= explorer[0]
+        console.log(explorer1);
+        FizzbuzzService.applyValidationInExplore(explorer1)
+        console.log(explorer1);
+        expect(explorer1.score).toBe(1); 
+        
+    });
+});
