cad613327ee316a84d922c5710614958f469ec8d
ChristianDeM <Christian.d.martino@gmail.com>
2022-05-01 16:58:14 -0500

    Creando el server basic

---
+++ libs/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+const port = 3000 // puerto para el server
+
+app.get('/', (req, res)=>{
+    res.send("hola a todos ")
+})
+
+//metodos de http  GET POST  PUT  DELATE
+app.get('/v1/explorers', (req,res)=>{
+    console.log(`GET explorers  api v1 ${new Date()}`)
+    const explorer1 ={ id:1,Name:"explorer1"}
+    const explorer2 ={ id:1,Name:"explorer2"}
+    const explorers=[explorer1,explorer2]
+    //HTTP ESTATUS:200 TODO VA BIEN
+    res.status(200).json(explorers)
+
+})
+app.listen(port,()=>{
+    console.log("server listo calisto¡");
+})
